7210b4a54e5cec3088903f02b868930b573d2ff7
Yusuke Kato <tq.github02480tq@gmail.com>
2017-05-18 08:51:07 +0000

    sample code

---
+++ MonteCalroMethod/LineDraw.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour {
+
+	GameObject Line1;
+	GameObject Line2;
+	GameObject Line3;
+	GameObject Line4;
+	LineRenderer LineRen1;
+	LineRenderer LineRen2;
+	LineRenderer LineRen3;
+	LineRenderer LineRen4;
+
+	// Use this for initialization
+	void Start () {
+		Line1 = GameObject.Find ("Line1");
+		Line2 = GameObject.Find ("Line2");
+		Line3 = GameObject.Find ("Line3");
+		Line4 = GameObject.Find ("Line4");
+		LineRen1 = Line1.GetComponent<LineRenderer> ();
+		LineRen2 = Line2.GetComponent<LineRenderer> ();
+		LineRen3 = Line3.GetComponent<LineRenderer> ();
+		LineRen4 = Line4.GetComponent<LineRenderer> ();
+		LineRen1.SetWidth (0.1f, 0.1f);
+		LineRen1.SetVertexCount (5);
+		LineRen1.SetPosition (0, new Vector3 (-1.0f, 1.0f, 0));
+		LineRen1.SetPosition (1, new Vector3 (1.0f, 1.0f, 0));
+		LineRen1.SetPosition (2, new Vector3 (1.0f, -1.0f, 0));
+		LineRen1.SetPosition (3, new Vector3 (-1.0f, -1.0f, 0));
+		LineRen1.SetPosition (4, new Vector3 (-1.0f, 1.0f, 0));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MonteCalroMethod/pointGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pointGenerator : MonoBehaviour {
+
+	public GameObject pointIn;
+	public GameObject pointOut;
+
+	const int TIMES = 100000;
+	const float range = 1.0f;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		int InCount = 0;
+		int OutCount = 0;
+		for (int j = 0; j < TIMES; j++) {
+			float x = Random.Range (-range, range);
+			float y = Random.Range (-range, range);
+			if (Mathf.Sqrt (Mathf.Pow (x, 2.0f) + Mathf.Pow (y, 2.0f)) <= range) {
+				InCount++;
+				GameObject go = (GameObject)Instantiate (pointIn);
+				go.transform.position = new Vector3 (x, y, 0);
+			} else {
+				OutCount++;
+				GameObject go2 = (GameObject)Instantiate (pointOut);
+				go2.transform.position = new Vector3 (x, y, 0);
+			}
+		}
+		float pi = 4.0f * (float)InCount / ((float)InCount + (float)OutCount);
+		text.text = "点数 : " + TIMES.ToString() + "\n" + "円周率 : " + pi.ToString () + "\n" + "InCount : " + InCount.ToString () + "\n" + "OutCount : " + OutCount.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
